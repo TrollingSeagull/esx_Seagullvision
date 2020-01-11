02b09ccc46c7ca728f0e23cff1dc47f72f8e17a4
TrollingSeagull <39966466+TrollingSeagull@users.noreply.github.com>
2020-01-11 02:36:00 +0100

    Update server.lua

--- esx_Seagullvision/server.lua
+++ esx_Seagullvision/server.lua
@@ -11,17 +11,3 @@ ESX.RegisterUsableItem('Thermalvision', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	  TriggerClientEvent('Thermalvision', source)
 end)
-
-RegisterNetEvent('night-thermal')
-AddEventHandler('night-thermal', function()
-	Citizen.CreateThread(function()
-
-    RequestAnimDict('anim@mp_helmets@on_bike@sports')
-
-    while not HasAnimDictLoaded('anim@mp_helmets@on_bike@sports') do
-      Citizen.Wait(0)
-    end
-
-		TaskPlayAnim(GetPlayerPed(-1), 'anim@mp_helmets@on_bike@sports')
-	end)	
-end)
